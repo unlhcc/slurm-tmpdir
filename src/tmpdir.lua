6386a7d59cfd82d7442e2b51e41d3984fe446f4f
jthiltges <jthiltges2@unl.edu>
2020-05-01 09:54:12 -0500

    Update tmpdir.lua to match current HCC production

--- src/tmpdir.lua
+++ src/tmpdir.lua
@@ -15,7 +15,7 @@ At job start:
 1) Create per-job temporary directories:
    a) /scratch/slurm_<jobid>
    b) /dev/shm/slurm_<jobid>
-2) Unshare namespaces
+2) Unshare namespaces:
    a) unshare CLONE_NEWNS
    b) unshare CLONE_NEWIPC
 3) Bind mount directories for job:
@@ -24,6 +24,11 @@ At job start:
      /var/tmp => /scratch/slurm_<jobid>/.var.tmp
      /dev/shm => /dev/shm/slurm_<jobid>
 4) Set $TMPDIR = /tmp
+5) Licensed FS access:
+   a) Access to lic_fs_path_name requires a Slurm
+      --licenses=lic_fs_license_name argument
+      If the job is missing the license, also bind mount:
+      /lic_fs_path_name => /scratch/slurm_<jobid>/.lic_fs_path_name
 
 At job completion:
 1) Remove /scratch/slurm_<jobid>
@@ -35,12 +40,40 @@ local p = require 'posix'
 
 local scratch = '/scratch'
 
+local lic_fs_license_name = 'common'
+local lic_fs_path_name = 'common'
+
 function job_scratch (spank, base)
   -- Job scratch is based on job ID
   local js = string.format('%s/slurm_%s', base,
     spank:get_item('S_JOB_ID')
   )
   return js
+end
+
+--[[
+Nabbed from:
+https://stackoverflow.com/questions/132397/get-back-the-output-of-os-execute-in-lua
+--]]
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+function request_licensed_fs(spank, ln)
+  local s_job_id = spank:get_item('S_JOB_ID')
+  local squeue = os.capture('/usr/bin/squeue -h -o %W -j ' .. s_job_id, false)
+  i, j = string.find(squeue, ln)
+  if i ~= nil then
+    return true
+  end
+  return false
 end
 
 -- Called for each task just after fork,
@@ -56,10 +89,16 @@ function slurm_spank_task_init_privileged (spank)
 
   SPANK.log_info("Using job scratch directory of %s", js)
 
+  -- Do we have a license for file system access?
+  lic_fs = request_licensed_fs(spank, lic_fs_license_name)
+
   -- Make job scratch directories
   p.mkdir(js)
   p.mkdir(js .. '/.tmp')
   p.mkdir(js .. '/.var.tmp')
+  if not lic_fs then
+    p.mkdir(js .. '/.' .. lic_fs_path_name)
+  end
   -- Make job shared memory directory
   p.mkdir(jm)
 
@@ -85,9 +124,15 @@ function slurm_spank_task_init_privileged (spank)
   s.unshare(s.CLONE_NEWNS)
   s.unshare(s.CLONE_NEWIPC)
 
+  -- poettering https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=739593
+  s.mount('/',               '/',        'none', (s.MS_REC + s.MS_SLAVE))
+
   -- Bind mount the new targets
   s.mount(js .. '/.tmp',     '/tmp',     'none', s.MS_BIND)
   s.mount(js .. '/.var.tmp', '/var/tmp', 'none', s.MS_BIND)
+  if not lic_fs then
+    s.mount(js .. '/.' .. lic_fs_path_name, '/' .. lic_fs_path_name, 'none', s.MS_BIND)
+  end
   s.mount(js,                '/scratch', 'none', s.MS_BIND)
   s.mount(jm,                '/dev/shm', 'none', s.MS_BIND)
 
